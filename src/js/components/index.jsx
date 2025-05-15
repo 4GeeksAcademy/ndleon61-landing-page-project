032e7e688d81d31a81a70df8d82c581e69037f15
David Leon <ndleon61@gmail.com>
2025-05-15 01:08:54 +0000

    added component folders

---
+++ src/js/components/index.jsx
@@ -0,0 +1,3 @@
+export {default as Jumbotron} from './jumbotron/Jumbotron';
+export {default as Card} from './card/Card';
+export {default as NavBar} from './navbar/Navbar';
---
+++ src/js/components/jumbotron/Jumbotron.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './jumbotron.css';
+
+export const Jumbotron = () => {
+  return (
+    <div>Jumnbotron</div>
+  )
+}
+
+export default Jumbotron;
